ff4714bcfff15306727d455d206a5a74f5b1ea20
Vinicius-PR <vinicius_auto@outlook.com>
2024-10-02 11:27:54 -0300

    ensure LoginRouter return 500 if an invalid httpRequest is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,7 +2,13 @@ const { StatusCodes } = require('http-status-codes')
 
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR
+      }
+    }
+    const { email, password } = httpRequest
+    if (!email || !password) {
       return {
         statusCode: StatusCodes.BAD_REQUEST
       }
@@ -31,5 +37,18 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
+  })
+
+  test('Should return 500 (INTERNAL_SERVER_ERROR) if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+  })
+
+  test('Should return 500 (INTERNAL_SERVER_ERROR) if httpRequest have no body', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
   })
 })
