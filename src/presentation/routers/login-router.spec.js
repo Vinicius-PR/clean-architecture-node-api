73825181534b248a5825338e56b73cbc8bdf1dde
Vinicius-PR <vinicius_auto@outlook.com>
2024-10-04 08:36:23 -0300

    Ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -73,4 +73,32 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(StatusCodes.UNAUTHORIZED)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+  test('Should return 500 (INTERNAL_SERVER_ERROR) if no AuthUseCase is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+  })
+
+  test('Should return 500 (INTERNAL_SERVER_ERROR) if no AuthUseCase has no auth method', () => {
+    class AuthUseCaseSpy {} // An empty class. With no method.
+    const sut = new LoginRouter(AuthUseCaseSpy)
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+  })
 })
